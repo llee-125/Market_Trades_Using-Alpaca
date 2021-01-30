be563e6f2326d136207fa187e3ce97e10d9f7eb9
llee-125 <llee@berkeley.edu>
2021-01-30 10:55:56 -0800

    First Commit

---
+++ stream.py
@@ -0,0 +1,36 @@
+import config
+
+import websocket, json
+from config import *
+
+def on_open(ws):
+    print("opened")
+    # auth_data = {
+    #     "action": "auth",
+    #     "params": API_KEY
+    # }
+
+#     ws.send(json.dumps(auth_data))
+
+#     channel_data = {
+#         "action": "subscribe",
+#         "params": TICKERS
+#     }
+
+#     ws.send(json.dumps(channel_data))
+
+
+# def on_message(ws, message):
+#     print("received a message")
+#     print(message)
+
+# def on_close(ws):
+#     print("closed connection")
+
+socket = "wss://data.alpaca.markets/stream"
+
+ws = websocket.WebSocketApp(socket, on_open=on_open)
+ws.run_forever()
+
+{"action": "authenticate","data": {"key_id": "AKMDKIKZEOS87BVEU5R3", "secret_key": "qONolMZhwourNxvNLObjjOwfXBlrMenLOhAGWxWd"}}
+#{"action": "listen", "data": {"streams": ["AM.SPY"]}}
